d3e4a5e8abbad078f6719986af8d029d1e222ac6
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 18:11:53 +0700

    Union sets using union() method

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ if __name__ == "__main__":
   print(s1)
   print(s2)
   print(s1.union(s2))
+
+  # Union sets using union() method
+
+  """
+  new_set = set.union(another_set, ...)
+  """
+
+  s1 = { "Python", "Java" }
+  s2 = { "C#", "Java" }
+  s = s1.union(s2)
+
+  print(s1)
+  print(s2)
+  print(s)
